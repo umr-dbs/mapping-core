4b962185cd09ab1de8fdc00ae7af061a783f59be
hoffsted <hoffsted@mathematik.uni-marburg.de>
2017-05-29 13:33:36 +0200

    Cleaned up gdal_source, basics working

--- operators/source/gdal_source.cpp
+++ operators/source/gdal_source.cpp
@@ -5,6 +5,9 @@
 #include "operators/operator.h"
 #include "util/configuration.h"
 
+#include "datatypes/raster/raster_priv.h"
+#include "util/gdal.h"
+
 #include <memory>
 #include <sstream>
 #include <cmath>
@@ -29,19 +32,17 @@ class RasterGDALSourceOperator : public GenericOperator {
 		int channel;
 		bool transform;
 		std::unique_ptr<GenericRaster> loadDataset( const char *filename, 
-													int rasterid, 
-													bool &flipx, bool &flipy, 
+													int rasterid, 													
 													epsg_t epsg, 
 													bool clip, 
-													double x1, double y1, 
-													double x2, double y2);
+													const QueryRectangle &qrect);
 		
 		std::unique_ptr<GenericRaster> loadRaster(GDALDataset *dataset, int rasteridx, double origin_x, double origin_y, 
-																					   double scale_x, double scale_y, 
-																					   bool &flipx, bool &flipy, 
+																					   double scale_x, double scale_y, 																					   
 																					   epsg_t default_epsg, bool clip, 
 																					   double clip_x1, double clip_y1, 
-																					   double clip_x2, double clip_y2);
+																					   double clip_x2, double clip_y2,
+																					   const QueryRectangle &qrect);
 
 };
 
@@ -75,15 +76,6 @@ void RasterGDALSourceOperator::getProvenance(ProvenanceCollection &pc) {
 		*/
 }
 
-
-std::unique_ptr<GenericRaster> RasterGDALSourceOperator::getRaster(const QueryRectangle &rect, const QueryTools &tools) {
-	// soll der operator komplett hier implementiert werden? 
-
-	bool flipX = false, flipY = false;
-	
-	return loadDataset(sourcename.c_str(), channel, flipX, flipY, rect->epsg, )
-}
-
 void RasterGDALSourceOperator::writeSemanticParameters(std::ostringstream &stream) {
 	std::string trans = transform ? "true" : "false";
 	stream << "{\"sourcename\": \"" << sourcename << "\",";
@@ -91,26 +83,24 @@ void RasterGDALSourceOperator::writeSemanticParameters(std::ostringstream &strea
 	stream << " \"transform\": " << trans << "}";
 }
 
+std::unique_ptr<GenericRaster> RasterGDALSourceOperator::getRaster(const QueryRectangle &rect, const QueryTools &tools) {	
+	return loadDataset(sourcename.c_str(), channel, rect.epsg, true, rect);
+}
 
-
-std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadRaster(GDALDataset *dataset, int rasteridx, double origin_x, double origin_y, double scale_x, double scale_y, bool &flipx, bool &flipy, epsg_t default_epsg, bool clip, double clip_x1, double clip_y1, double clip_x2, double clip_y2) {
+std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadRaster(GDALDataset *dataset, int rasteridx, double origin_x, double origin_y, double scale_x, double scale_y, epsg_t default_epsg, bool clip, double clip_x1, double clip_y1, double clip_x2, double clip_y2, const QueryRectangle& qrect) {
 	GDALRasterBand  *poBand;
 	int             nBlockXSize, nBlockYSize;
 	int             bGotMin, bGotMax;
 	double          adfMinMax[2];
+
+	bool flipx = false, flipy = false;
 
 	poBand = dataset->GetRasterBand( rasteridx );
 	poBand->GetBlockSize( &nBlockXSize, &nBlockYSize );
 
 	GDALDataType type = poBand->GetRasterDataType();
 
-/*
-	printf( "Block=%dx%d Type=%s, ColorInterp=%s\n",
-		nBlockXSize, nBlockYSize,
-		GDALGetDataTypeName(poBand->GetRasterDataType()),
-		GDALGetColorInterpretationName(
-				poBand->GetColorInterpretation()) );
-*/
+
 	adfMinMax[0] = poBand->GetMinimum( &bGotMin );
 	adfMinMax[1] = poBand->GetMaximum( &bGotMax );
 	if( ! (bGotMin && bGotMax) )
@@ -123,21 +113,6 @@ std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadRaster(GDALDataset 
 		hasnodata = false;
 		nodata = 0;
 	}
-	/*
-	if (nodata < adfMinMax[0] || nodata > adfMinMax[1]) {
-
-	}
-	*/
-/*
-	printf( "Min=%.3g, Max=%.3g\n", adfMinMax[0], adfMinMax[1] );
-
-	if( poBand->GetOverviewCount() > 0 )
-		printf( "Band has %d overviews.\n", poBand->GetOverviewCount() );
-
-	if( poBand->GetColorTable() != NULL )
-		printf( "Band has a color table with %d entries.\n",
-			poBand->GetColorTable()->GetColorEntryCount() );
-*/
 
 	// Figure out the data type
 	epsg_t epsg = default_epsg;
@@ -151,7 +126,6 @@ std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadRaster(GDALDataset 
 		type = GDT_Int16; // TODO: sollte GDT_UInt16 sein!
 	}
 
-	// Figure out which pixels to load
 	int   nXSize = poBand->GetXSize();
 	int   nYSize = poBand->GetYSize();
 
@@ -180,71 +154,43 @@ std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadRaster(GDALDataset 
 		pixel_height = pixel_y2 - pixel_y1 + 1;
 	}
 
-	double x1 = origin_x + scale_x * (pixel_x1 - 0.5);
-	double y1 = origin_y + scale_y * (pixel_y1 - 0.5);
+	double x1 = origin_x + scale_x * (pixel_x1 - 0.5) + 0.05;
+	double y1 = origin_y + scale_y * (pixel_y1 - 0.5) - 0.05;
 	double x2 = x1 + scale_x * pixel_width;
-	double y2 = y1 + scale_y * pixel_height;
+	double y2 = y1 + scale_y * pixel_height - 0.05;
+
+	const TemporalReference &tref = (const TemporalReference &)qrect;
 
 	SpatioTemporalReference stref(
-		SpatialReference(epsg, x1, y1, x2, y2, flipx, flipy),
-		TemporalReference::unreferenced()
+		SpatialReference(qrect.epsg, x1, y1, x2, y2, flipx, flipy),
+		TemporalReference(tref.timetype, tref.t1, tref.t2)		
 	);
 	Unit unit = Unit::unknown();
 	unit.setMinMax(minvalue, maxvalue);
 	DataDescription dd(type, unit, hasnodata, nodata);
-	//printf("loading raster with %g -> %g valuerange\n", adfMinMax[0], adfMinMax[1]);
 
 	auto raster = GenericRaster::create(dd, stref, pixel_width, pixel_height);
 	void *buffer = raster->getDataForWriting();
-	//int bpp = raster->getBPP();
 
-	/*
-CPLErr GDALRasterBand::RasterIO( GDALRWFlag eRWFlag,
-                                 int nXOff, int nYOff, int nXSize, int nYSize,
-                                 void * pData, int nBufXSize, int nBufYSize,
-                                 GDALDataType eBufType,
-                                 int nPixelSpace,
-                                 int nLineSpace )
-	*/
+	
+	GDALRasterIOExtraArg *extraArg = NULL; //new GDALRasterIOExtraArg;
+	//extraArg->nVersion = 0; //without settings this its throwing an "Importer Exception" with "Unhandled version of GDALRasterIOExtraArg" Whit setting it its an segmentation fault
+	//extraArg->eResampleAlg = GRIORA_Bilinear;
+	
 
 	// Read Pixel data
 	auto res = poBand->RasterIO( GF_Read,
 		pixel_x1, pixel_y1, pixel_width, pixel_height, // rectangle in the source raster
 		buffer, raster->width, raster->height, // position and size of the destination buffer
-		type, 0, 0);
+		type, 0, 0, extraArg);	//NULL instead of extraArg Pointer. &extraArg
 
 	if (res != CE_None)
 		throw ImporterException("GDAL: RasterIO failed");
 
-	// Selectively read metadata
-	//char **mdList = GDALGetMetadata(poBand, "msg");
-	if (epsg == EPSG_GEOSMSG) {
-		char **mdList = poBand->GetMetadata("msg");
-		for (int i = 0; mdList && mdList[i] != nullptr; i++ ) {
-			//printf("GDALImport: got Metadata %s\n", mdList[i]);
-			std::string md(mdList[i]);
-			size_t split = md.find('=');
-			if (split == std::string::npos)
-				continue;
-
-			std::string key = md.substr(0, split);
-			std::string mkey = "msg." + key;
-			std::string value = md.substr(split+1, std::string::npos);
-
-			double dvalue = std::strtod(value.c_str(), nullptr);
-			if (key == "TimeStamp" || (dvalue == 0 && value != "0")) {
-				raster->global_attributes.setTextual(mkey, value);
-			}
-			else {
-				raster->global_attributes.setNumeric(mkey, dvalue);
-			}
-		}
-	}
-
 	return raster;
 }
 
-std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadDataset(const char *filename, int rasterid, bool &flipx, bool &flipy, epsg_t epsg, bool clip, double x1, double y1, double x2, double y2) {
+std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadDataset(const char *filename, int rasterid, epsg_t epsg, bool clip, const QueryRectangle &qrect) {
 	GDAL::init();
 
 	GDALDataset *dataset = (GDALDataset *) GDALOpen(filename, GA_ReadOnly);
@@ -252,19 +198,6 @@ std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadDataset(const char 
 	if (dataset == NULL)
 		throw ImporterException(concat("Could not open dataset ", filename));
 
-
-/*
-	printf( "Driver: %s/%s\n",
-		dataset->GetDriver()->GetDescription(),
-		dataset->GetDriver()->GetMetadataItem( GDAL_DMD_LONGNAME ) );
-
-	printf( "Size is %dx%dx%d\n",
-		dataset->GetRasterXSize(), dataset->GetRasterYSize(),
-		dataset->GetRasterCount() );
-
-	if( dataset->GetProjectionRef()  != NULL )
-			printf( "Projection is `%s'\n", dataset->GetProjectionRef() );
-*/
 	double adfGeoTransform[6];
 
 	// http://www.gdal.org/classGDALDataset.html#af9593cc241e7d140f5f3c4798a43a668
@@ -272,23 +205,8 @@ std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadDataset(const char 
 		GDALClose(dataset);
 		throw ImporterException("no GeoTransform information in raster");
 	}
-/*
-	if (adfGeoTransform[2] != 0 || adfGeoTransform[4] != 0) {
-		GDALClose(dataset);
-		std::stringstream ss;
-		ss << "Raster is using an unsupported GeoTransform: (" << adfGeoTransform[0] << "/" << adfGeoTransform[1] << "/" << adfGeoTransform[2] << "/" << adfGeoTransform[3] << ")";
-		throw ImporterException(ss.str());
-	}
-*/
-/*
-	{
-		printf( "Origin = (%.6f,%.6f)\n",
-			adfGeoTransform[0], adfGeoTransform[3] );
 
-		printf( "Pixel Size = (%.6f,%.6f)\n",
-			adfGeoTransform[1], adfGeoTransform[5] );
-	}
-*/
+	printf("GeoTransform: %f, %f, %f, %f, %f, %f \n", adfGeoTransform[0], adfGeoTransform[1], adfGeoTransform[2], adfGeoTransform[3], adfGeoTransform[4], adfGeoTransform[5], adfGeoTransform[6] );
 
 	int rastercount = dataset->GetRasterCount();
 
@@ -297,16 +215,7 @@ std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadDataset(const char 
 		throw ImporterException("rasterid not found");
 	}
 
-
-	const char *drivername = dataset->GetDriver()->GetDescription();
-	//const char *drivername = dataset->GetDriverName();
-	//printf("Driver: %s\n", drivername);
-	if (strcmp(drivername, "MSG") == 0) {
-		if (epsg != EPSG_GEOSMSG)
-			throw ImporterException("MSG driver can only import rasters in MSG projection");
-	}
-
-	auto raster = GDALImporter_loadRaster(dataset, rasterid, adfGeoTransform[0], adfGeoTransform[3], adfGeoTransform[1], adfGeoTransform[5], flipx, flipy, epsg, clip, x1, y1, x2, y2);
+	auto raster = loadRaster(dataset, rasterid, adfGeoTransform[0], adfGeoTransform[3], adfGeoTransform[1], adfGeoTransform[5], epsg, clip, qrect.x1, qrect.y1, qrect.x2, qrect.y2, qrect);
 
 	GDALClose(dataset);
 
