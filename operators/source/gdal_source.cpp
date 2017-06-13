7a76ac7da575c636aefe13ae438924425c51fe3c
hoffsted <hoffsted@mathematik.uni-marburg.de>
2017-06-13 11:17:14 +0200

    Work in progress: structure for finding right raster file, snapping of to time interval of dataset not implemented

--- operators/source/gdal_source.cpp
+++ operators/source/gdal_source.cpp
@@ -4,15 +4,19 @@
 #include "raster/opencl.h"
 #include "operators/operator.h"
 #include "util/configuration.h"
+#include "util/timeparser.h"
 
 #include "datatypes/raster/raster_priv.h"
 #include "util/gdal.h"
 
 #include <memory>
+#include <iostream>
 #include <sstream>
+#include <fstream>
 #include <cmath>
 #include <json/json.h>
-
+#include <ctime>
+#include <dirent.h>
 
 /**
  * Operator that gets raster data from file via gdal
@@ -28,9 +32,29 @@ class RasterGDALSourceOperator : public GenericOperator {
 	protected:
 		void writeSemanticParameters(std::ostringstream &stream);
 	private:
+
+		const std::string DATASET_PATH = "test/systemtests/data/gdal_files";
+
+		enum class TimeUnit {
+		    Year 	= 0,
+		    Month 	= 1,
+		    Day 	= 2,
+		    Hour 	= 3,
+			Minute  = 4,
+		    Second  = 5
+		};
+		
+		static const std::map<std::string, RasterGDALSourceOperator::TimeUnit> string_to_TimeUnit;
+		static TimeUnit createTimeUnit(std::string value);
+
+
+		int maxValueForTimeUnit(TimeUnit part) const;
+		std::string unixTimeToString(double unix_time, std::string format);
+
 		std::string sourcename;
 		int channel;
 		bool transform;
+
 		std::unique_ptr<GenericRaster> loadDataset( std::string filename, 
 													int rasterid, 													
 													epsg_t epsg, 
@@ -43,7 +67,9 @@ class RasterGDALSourceOperator : public GenericOperator {
 																					   double clip_x1, double clip_y1, 
 																					   double clip_x2, double clip_y2,
 																					   const QueryRectangle &qrect);
-
+		Json::Value getDatasetJson(std::string datasetName);
+		std::string getDatasetFilename(Json::Value datasetJson, double wantedTimeUnix);
+		std::string snapToInterval(RasterGDALSourceOperator::TimeUnit unit, int unitValue, std::string time, std::string format);		
 };
 
 
@@ -83,9 +109,11 @@ void RasterGDALSourceOperator::writeSemanticParameters(std::ostringstream &strea
 	stream << " \"transform\": " << trans << "}";
 }
 
-std::unique_ptr<GenericRaster> RasterGDALSourceOperator::getRaster(const QueryRectangle &rect, const QueryTools &tools) {		
-	auto raster = loadDataset(sourcename, channel, rect.epsg, true, rect);	
-	//flip here because the tiff result will be flipped else
+std::unique_ptr<GenericRaster> RasterGDALSourceOperator::getRaster(const QueryRectangle &rect, const QueryTools &tools) {
+	Json::Value datasetJson = getDatasetJson(sourcename);
+	std::string file_path = getDatasetFilename(datasetJson, rect.t1);
+	auto raster = loadDataset(file_path, channel, rect.epsg, true, rect);	
+	//flip here so the tiff result will not be flipped
 	return raster->flip(false, true);
 }
 
@@ -215,3 +243,142 @@ std::unique_ptr<GenericRaster> RasterGDALSourceOperator::loadDataset(std::string
 	return raster;
 }
 
+std::string RasterGDALSourceOperator::getDatasetFilename(Json::Value datasetJson, double wantedTimeUnix){
+
+	// ISO 8601 timestamp: 2017-06-12T07:40:16Z	
+	
+	std::string time_format = datasetJson.get("time_format", "%Y-%m-%d").asString();
+	std::string time_start 	= datasetJson.get("time_start", "").asString();
+	std::string time_end 	= datasetJson.get("time_end", "").asString();
+    
+    auto timeParser = TimeParser::createCustom(time_format); 
+
+    //check if requested time is in range of dataset timestamps
+	double startUnix 	= timeParser->parse(time_start);
+	double endUnix 		= timeParser->parse(time_end);
+	if(wantedTimeUnix < startUnix || wantedTimeUnix > endUnix){
+		throw new OperatorException("Requested time is not in range of dataset");
+	}
+
+	Json::Value time_interval = datasetJson.get("time_interval", NULL);
+
+	TimeUnit interval_unit = createTimeUnit(time_interval.get("unit", "Month").asString());
+	int interval_value = time_interval.get("value", 1).asInt();
+	
+	if(interval_unit != TimeUnit::Year){
+		std::cout << maxValueForTimeUnit(interval_unit) % interval_value << std::endl;
+		if(maxValueForTimeUnit(interval_unit) % interval_value != 0){
+			throw OperatorException("GDALSource: dataset invalid, interval value modulo unit-length must be 0. e.g. 4 % 12, for Months, or 30%60 for Minutes");
+		}
+	} else {
+		if(interval_value != 1){
+			throw OperatorException("GDALSource: dataset invalid, interval value for interval unit Year has to be 1.");	
+		}
+	}
+
+	std::string wantedTimeFormated = unixTimeToString(wantedTimeUnix, time_format); //do i need this?
+	time_t wantedTimeTimet = startUnix;
+	tm wantedTimeTm = *gmtime(&wantedTimeUnix);
+
+	time_t startTimeTimet = startUnix;
+	tm startTimeTm = *gmtime(&startTimeTimet);
+
+
+	tm snapped_time = snapToInterval(interval_unit, interval_value, startTimeTm, wantedTimeTm);
+
+	// todo: snapped time to string, with tm -> time_t -> unixToString
+
+	std::string path = datasetJson.get("path", "").asString();
+	std::string file_name = datasetJson.get("file_name", "").asString();
+
+	std::string placeholder = "%%%TIME_STRING%%%";
+	size_t placeholer_pos = file_name.find(placeholder);
+
+	file_name = file_name.replace(placeholer_pos, placeholder.length(), snapped_time_string);
+	return path + "/" + file_name;
+}
+
+tm RasterGDALSourceOperator::snapToInterval(RasterGDALSourceOperator::TimeUnit unit, int unitValue, tm startTime, tm wantedTime){
+	tm snapped;
+
+	// TODO
+	
+	return snapped;
+}
+
+Json::Value RasterGDALSourceOperator::getDatasetJson(std::string datasetName){
+	struct dirent *entry;
+	DIR *dir = opendir(DATASET_PATH.c_str());
+
+	if (dir == NULL) {
+        throw OperatorException("GDAL Source: directory for dataset json files does not exist.");
+    }
+
+	while ((entry = readdir(dir)) != NULL) {
+        std::string filename = entry->d_name;
+        std::string withoutExtension = filename.substr(0, filename.length() - 5);
+        
+        if(withoutExtension == datasetName){
+        	
+        	//open file then read json object from it
+        	std::ifstream file(DATASET_PATH + "/" + filename);
+			if (!file.is_open()) {
+			    closedir(dir);
+				throw OperatorException("GDAL Source Operator: unable to dataset file " + datasetName);
+			}
+
+			Json::Reader reader(Json::Features::strictMode());
+			Json::Value json;
+			if (!reader.parse(file, json)) {
+			    closedir(dir);
+				throw OperatorException("GDAL Source Operator: unable to read json" + reader.getFormattedErrorMessages());				
+			}				
+
+		    closedir(dir);
+        	return json;
+        }
+
+    }
+
+    closedir(dir);
+    throw OperatorException("GDAL Source: Dataset " + datasetName + " does not exist.");
+}
+
+const std::map<std::string, RasterGDALSourceOperator::TimeUnit> RasterGDALSourceOperator::string_to_TimeUnit {
+	{"Second", 	RasterGDALSourceOperator::TimeUnit::Second},
+	{"Minute", 	RasterGDALSourceOperator::TimeUnit::Minute},
+	{"Hour", 	RasterGDALSourceOperator::TimeUnit::Hour},
+	{"Day", 	RasterGDALSourceOperator::TimeUnit::Day},
+	{"Month", 	RasterGDALSourceOperator::TimeUnit::Month},
+	{"Year", 	RasterGDALSourceOperator::TimeUnit::Year}
+};
+
+RasterGDALSourceOperator::TimeUnit RasterGDALSourceOperator::createTimeUnit(std::string value) {
+	return string_to_TimeUnit.at(value);
+}
+
+int RasterGDALSourceOperator::maxValueForTimeUnit(RasterGDALSourceOperator::TimeUnit part) const {
+	switch(part){
+		case RasterGDALSourceOperator::TimeUnit::Year:
+			return 0;
+		case RasterGDALSourceOperator::TimeUnit::Month:
+			return 12;			
+		case RasterGDALSourceOperator::TimeUnit::Day:	//TODO: how to handle 31, 28 day months?
+			return 30;			
+		case RasterGDALSourceOperator::TimeUnit::Hour:
+			return 24;
+		case RasterGDALSourceOperator::TimeUnit::Minute:
+			return 60;
+		case RasterGDALSourceOperator::TimeUnit::Second:
+			return 60;
+	}
+}
+
+std::string RasterGDALSourceOperator::unixTimeToString(double unix_time, std::string format){
+	time_t tt = unix_time;
+	time_t t = time(&tt);
+	struct tm *tm = localtime(&t);
+	char date[20];	//max length of a time string is 19 + zero escape
+	strftime(date, sizeof(date), format.c_str(), tm);
+	return std::string(date);
+}
