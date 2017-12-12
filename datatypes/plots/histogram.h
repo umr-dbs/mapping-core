2a58ac12a3701141299517cf37b17f011fc750f9
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2017-12-12 11:40:43 +0100

    refactored histogram plot data type

--- datatypes/plots/histogram.h
+++ datatypes/plots/histogram.h
@@ -11,60 +11,68 @@
  * This class models a one dimensional histograms with variable number of buckets
  */
 class Histogram : public GenericPlot {
-	public:
-		Histogram(int number_of_buckets, double min, double max);
-		Histogram(BinaryReadBuffer &buffer);
+    public:
+        Histogram(unsigned long number_of_buckets, double min, double max);
 
-		virtual ~Histogram();
+        explicit Histogram(BinaryReadBuffer &buffer);
 
-		void inc(double value);
-		void incNoData();
+        ~Histogram() override;
 
-		const std::string toJSON() const;
+        void inc(double value);
 
-		static const int DEFAULT_NUMBER_OF_BUCKETS = 10000;
+        void incNoData();
 
-		int getCountForBucket(int bucket){
-			return counts.at(bucket);
-		}
-		int getNoDataCount(){
-			return nodata_count;
-		}
-		double getMin(){
-			return min;
-		}
-		double getMax(){
-			return max;
-		}
-		int getNumberOfBuckets(){
-			return counts.size();
-		}
-		/*
-		 * returns the count of all inserted elements (without NoData)
-		 */
-		int getValidDataCount();
-		/**
-		 * calculates the bucket where a value would be inserted
-		 */
-		int calculateBucketForValue(double value);
-		/**
-		 * calculates the bucket minimum
-		 */
-		double calculateBucketLowerBorder(int bucket);
-		/**
-		 * add a marker
-		 */
-		void addMarker(double bucket, const std::string &label);
+        const std::string toJSON() const override;
 
-		std::unique_ptr<GenericPlot> clone() const;
+        int getCountForBucket(unsigned long bucket) {
+            return counts.at(bucket);
+        }
 
-		virtual void serialize(BinaryWriteBuffer &buffer, bool is_persistent_memory) const;
+        int getNoDataCount() {
+            return nodata_count;
+        }
 
-	private:
-		std::vector<int> counts;
-		int nodata_count;
-		double min, max;
-		std::vector<std::pair<double, std::string>> markers;
+        double getMin() {
+            return min;
+        }
+
+        double getMax() {
+            return max;
+        }
+
+        unsigned long getNumberOfBuckets() {
+            return counts.size();
+        }
+
+        /*
+         * returns the count of all inserted elements (without NoData)
+         */
+        int getValidDataCount();
+
+        /**
+         * calculates the bucket where a value would be inserted
+         */
+        int calculateBucketForValue(double value);
+
+        /**
+         * calculates the bucket minimum
+         */
+        double calculateBucketLowerBorder(int bucket);
+
+        /**
+         * add a marker
+         */
+        void addMarker(double bucket, const std::string &label);
+
+        std::unique_ptr<GenericPlot> clone() const override;
+
+        void serialize(BinaryWriteBuffer &buffer, bool is_persistent_memory) const override;
+
+    private:
+        std::vector<int> counts;
+        int nodata_count;
+        double min, max;
+        std::vector<std::pair<double, std::string>> markers;
 };
 
 #endif
