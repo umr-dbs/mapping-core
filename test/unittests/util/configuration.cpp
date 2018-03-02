95bd15e4b082e7a6ac9089b6089ad5f67afab9d8
Sören Hoffstedt <hoffsted@students.uni-marburg.de>
2018-03-02 14:33:54 +0100

    Added unit test to test that cpptoml can return a size_t.

--- test/unittests/util/configuration.cpp
+++ test/unittests/util/configuration.cpp
@@ -5,7 +5,7 @@
 TEST(Configuration, mergeTest){
 
     Configuration::loadFromDefaultPaths();
-    Configuration::loadFromString("test=123\ntest2=true\ntest3=1.56\n[test4]\nsubTest=true");
+    Configuration::loadFromString("test=123\ntest2=true\ntest3=1.56\ntestSizet=1231231244234\n[test4]\nsubTest=true");
     Configuration::loadFromFile("../../test/unittests/util/test_config.toml");
 
     EXPECT_EQ(Configuration::get<int>("test"), 123);
@@ -13,6 +13,10 @@ TEST(Configuration, mergeTest){
     EXPECT_EQ(Configuration::get<double>("test3"), 1.56);
     EXPECT_EQ(Configuration::get<bool>("test4.subTest"), true);
     EXPECT_EQ(Configuration::get<double>("notExists", 1.75), 1.75);
+
+    size_t t1 = Configuration::get<size_t>("testSizet");
+    size_t exp = 1231231244234;
+    EXPECT_EQ(t1,exp);
 
     //Variables from file :
     EXPECT_EQ(Configuration::get<bool>("someFlag"), false);
