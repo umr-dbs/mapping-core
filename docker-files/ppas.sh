da1910580abfaabe7b6b32ca1b1e83fa35f53ec3
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2018-06-13 09:06:11 +0200

    dockerized mapping-core

---
+++ docker-files/ppas.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+## GDAL
+add-apt-repository ppa:ubuntugis/ppa
+## Poco
+add-apt-repository ppa:gezakovacs/poco
+## CMake >= 3.7
+add-apt-repository ppa:adrozdoff/cmake
+## CLang 5.0
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-5.0 main"
+
+apt-get update
