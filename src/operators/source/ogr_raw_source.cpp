eb3218b100ced636e3362cb094a668a62cd24c58
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2020-05-06 15:27:26 +0200

    resolve data uri as memory mapped file

--- src/operators/source/ogr_raw_source.cpp
+++ src/operators/source/ogr_raw_source.cpp
@@ -1,4 +1,3 @@
-#include <gdal/ogrsf_frmts.h>
 #include "operators/operator.h"
 #include "datatypes/polygoncollection.h"
 #include "datatypes/linecollection.h"
@@ -6,7 +5,7 @@
 #include "datatypes/simplefeaturecollection.h"
 #include "util/ogr_source_util.h"
 #include <json/json.h>
-#include <iostream>
+#include <Poco/URI.h>
 
 /** Operator for opening OGR/GDAL supported vector data files as FeatureCollections.
  *  Main implementation of feature reading can be found in util/ogr_source_util.h/cpp.
@@ -56,7 +55,7 @@ class OGRRawSourceOperator : public GenericOperator {
 
         auto getPointCollection(const QueryRectangle &rect, const QueryTools &tools) -> std::unique_ptr<PointCollection> override;
 
-        auto getLineCollection(const QueryRectangle &rect, const QueryTools &tools) ->  std::unique_ptr<LineCollection> override;
+        auto getLineCollection(const QueryRectangle &rect, const QueryTools &tools) -> std::unique_ptr<LineCollection> override;
 
         auto getPolygonCollection(const QueryRectangle &rect, const QueryTools &tools) -> std::unique_ptr<PolygonCollection> override;
 
@@ -65,15 +64,74 @@ class OGRRawSourceOperator : public GenericOperator {
 
         void getProvenance(ProvenanceCollection &pc) override;
 
-private:
-	std::unique_ptr<OGRSourceUtil> ogrUtil;
+    private:
+        static auto is_data_url(const std::string &filename) -> bool;
+
+        /// Create a GDAL Memfile and return its filename `/vsimem/<RANDOMCHARS>`
+        ///
+        /// The parts that require de-allocation are stored at `vsi_mem_file` and `vsi_mem_file_content`
+        auto create_memfile(const std::string &filename) -> std::string;
+
+        /// Swaps the `filename` of `params` with `vsi_mem_file_name`, calls `query_function` and swaps the old value back.
+        /// Returns the result of `query_function`.
+        template<typename T>
+        auto swap_call_and_return(std::function<T()> query_function) -> T;
+
+        std::unique_ptr<OGRSourceUtil> ogrUtil;
+
+        std::unique_ptr<VSILFILE, std::function<void(VSILFILE *)>> vsi_mem_file; // must be de-allocated if it is not null
+        std::unique_ptr<std::string> vsi_mem_file_content;
+        std::unique_ptr<std::string> vsi_mem_file_name;
 };
 
-OGRRawSourceOperator::OGRRawSourceOperator(int sourcecounts[], GenericOperator *sources[], Json::Value &params) : GenericOperator(sourcecounts, sources) {
-	assumeSources(0);
-	std::string local_id = "data.ogr_raw_source.";
-	local_id.append(params.get("filename", "").asString()); //todo: does this need the layer_name added?
-	ogrUtil = std::make_unique<OGRSourceUtil>(params, std::move(local_id));
+OGRRawSourceOperator::OGRRawSourceOperator(int sourcecounts[], GenericOperator *sources[], Json::Value &params)
+        : GenericOperator(sourcecounts, sources) {
+    assumeSources(0);
+
+    const auto filename = params.get("filename", "").asString();
+    std::string local_id;
+
+    if (is_data_url(filename)) {
+        vsi_mem_file_name = std::make_unique<std::string>(create_memfile(filename));
+        local_id = ""; // prevent security checks
+    } else {
+        //todo: does this need the layer_name added?
+        local_id = "data.ogr_raw_source." + filename;
+    }
+
+    ogrUtil = std::make_unique<OGRSourceUtil>(params, std::move(local_id));
+}
+
+auto OGRRawSourceOperator::create_memfile(const std::string &filename) -> std::string {
+    const auto comma_position = filename.find(',');
+
+    if (comma_position == std::string::npos) {
+        throw OperatorException("OGRRawSource: Invalid Data URI since it misses a comma");
+    }
+    if (comma_position >= filename.length()) {
+        throw OperatorException("OGRRawSource: Invalid Data URI since the data part is empty");
+    }
+
+    const auto data_encoded = filename.substr(comma_position + 1);
+    vsi_mem_file_content = std::make_unique<std::string>();
+
+    Poco::URI::decode(data_encoded, *vsi_mem_file_content, false);
+
+    auto mem_file_name = concat("/vsimem/", UserDB::createRandomToken(32));
+
+    auto *vsi_file = VSIFileFromMemBuffer(
+            mem_file_name.c_str(),
+            const_cast<GByte *>(reinterpret_cast<const GByte *>(vsi_mem_file_content->c_str())),
+            vsi_mem_file_content->length(),
+            false
+    );
+
+    vsi_mem_file = std::unique_ptr<VSILFILE, std::function<void(VSILFILE *)>>(
+            vsi_file,
+            [=](VSILFILE *vsi_file) { VSIUnlink(mem_file_name.c_str()); }
+    );
+
+    return mem_file_name;
 }
 
 REGISTER_OPERATOR(OGRRawSourceOperator, "ogr_raw_source"); // NOLINT(cert-err58-cpp)
@@ -89,13 +147,39 @@ void OGRRawSourceOperator::getProvenance(ProvenanceCollection &pc) {
 }
 
 auto OGRRawSourceOperator::getPointCollection(const QueryRectangle &rect, const QueryTools &tools) -> std::unique_ptr<PointCollection> {
-    return ogrUtil->getPointCollection(rect, tools);
+    return swap_call_and_return<std::unique_ptr<PointCollection> >([&]() {
+        return ogrUtil->getPointCollection(rect, tools);
+    });
 }
 
-auto OGRRawSourceOperator::getLineCollection(const QueryRectangle &rect, const QueryTools &tools) -> std::unique_ptr<LineCollection>{
-    return ogrUtil->getLineCollection(rect, tools);
+auto OGRRawSourceOperator::getLineCollection(const QueryRectangle &rect, const QueryTools &tools) -> std::unique_ptr<LineCollection> {
+    return swap_call_and_return<std::unique_ptr<LineCollection> >([&]() {
+        return ogrUtil->getLineCollection(rect, tools);
+    });
 }
 
 auto OGRRawSourceOperator::getPolygonCollection(const QueryRectangle &rect, const QueryTools &tools) -> std::unique_ptr<PolygonCollection> {
-    return ogrUtil->getPolygonCollection(rect, tools);
+    return swap_call_and_return<std::unique_ptr<PolygonCollection> >([&]() {
+        return ogrUtil->getPolygonCollection(rect, tools);
+    });
+}
+
+auto OGRRawSourceOperator::is_data_url(const std::string &filename) -> bool {
+    return filename.find("data:") == 0;
+}
+
+template<typename T>
+auto OGRRawSourceOperator::swap_call_and_return(std::function<T()> query_function) -> T {
+    if (vsi_mem_file_name) {
+        auto filename = ogrUtil->getParameters()["filename"].asString();
+        ogrUtil->getParameters()["filename"] = *vsi_mem_file_name;
+
+        T result = query_function();
+
+        ogrUtil->getParameters()["filename"] = filename;
+
+        return result;
+    }
+
+    return query_function();
 }
